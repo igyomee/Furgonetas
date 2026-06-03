efb683ba7449f8381ee733688fee523a2859de8b
igyomee <ig.yomee@gmail.com>
2026-06-03 15:46:19 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window.FLEET_CONFIG = {
+  companyName: "Mi empresa",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbwCh4CzM99AwciUCWF2UvghSPgnCsasWJi2uxLrZJX91xNx1tZDBWL8fINZpcaccJuPDg/exec",
+  publicToken: "",
+  vans: [
+    { id: "furgo-01", label: "Furgoneta 1", plate: "0000 AAA" },
+    { id: "furgo-02", label: "Furgoneta 2", plate: "1111 BBB" },
+    { id: "furgo-03", label: "Furgoneta 3", plate: "2222 CCC" }
+  ]
+};
