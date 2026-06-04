37d10b3160a8a8b30a01b8bcc5d4910b3a2e920e
igyomee <ig.yomee@gmail.com>
2026-06-04 08:37:08 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.FLEET_CONFIG = {
+  companyName: "Yomee",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbwCh4CzM99AwciUCWF2UvghSPgnCsasWJi2uxLrZJX91xNx1tZDBWL8fINZpcaccJuPDg/exec",
+  publicToken: "",
+  vans: window.FLEET_DATA ? window.FLEET_DATA.vans : [],
+  operators: window.FLEET_DATA ? window.FLEET_DATA.operators : []
+};
